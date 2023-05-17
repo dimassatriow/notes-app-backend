ee3921ad85e92c4399d646c94f9d8d2fcdce2333
Dimas <dimaswicak70@gmail.com>
2023-05-17 12:29:30 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const { 
+  addNoteHandler, 
+  getAllNotesHandler, 
+  getNoteByIdHandler, 
+  editNotesByIdHandler, 
+  deleteNotesByIdHandler
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNotesByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNotesByIdHandler,
+  }
+];
+
+module.exports = routes;
